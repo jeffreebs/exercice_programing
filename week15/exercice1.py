7e60edc9186906bc0b5b56bf6b4d698838f97a97
jeffreebs <berrocal.97@hotmail.com>
2025-06-01 08:32:14 -0600

    Two options of sorted

---
+++ week15/exercice1.py
@@ -0,0 +1,28 @@
+def bubble_sort_num(list):
+    l=len(list)
+    for n in range (l):
+        for x in range (0,l-n-1):
+            if list[x]>list[x+1]:
+                list[x],list[x+1]=list[x+1],list[x]
+    return list
+
+
+num= [1,2,3,4,5]
+sort=bubble_sort_num(num)
+print(sort)
+
+# def bubble_sort_num(list):
+#     l=len(list)
+#     for n in range (l):
+#         for x in range (l-1,n,-1):
+#             if list[x]<list[x-1]:
+#                 list[x],list[n-1]=list[x-1],list[n]
+#     return list
+
+
+# num= [1,2,3,4,5]
+# sort=bubble_sort_num(num)
+# print(sort)
+
+
+
