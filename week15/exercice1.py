ee44a1f1f156f5bb92759275d770be62f8b9c410
jeffreebs <berrocal.97@hotmail.com>
2025-06-02 15:27:54 -0600

    Chance applied

--- week15/exercice1.py
+++ week15/exercice1.py
@@ -11,18 +11,7 @@ num= [1,2,3,4,5]
 sort=bubble_sort_num(num)
 print(sort)
 
-# def bubble_sort_num(list):
-#     l=len(list)
-#     for n in range (l):
-#         for x in range (l-1,n,-1):
-#             if list[x]<list[x-1]:
-#                 list[x],list[n-1]=list[x-1],list[n]
-#     return list
 
-
-# num= [1,2,3,4,5]
-# sort=bubble_sort_num(num)
-# print(sort)
 
 
 
